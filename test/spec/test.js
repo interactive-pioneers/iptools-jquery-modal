c9c693049536dcb1af0fa86add3ed27dc6c70ada
Georg Meyer <gm@interactive-pioneers.de>
2015-05-27 10:32:00 +0200

    Read closure to tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,20 +1,20 @@
 'use strict';
-
 /* jshint undef: false */
+(function() {
+  describe('iptoolsModal', function() {
 
-describe('iptoolsModal', function() {
+    var modal = null;
 
-  var modal = null;
+    describe('init', function() {
+      
+      beforeEach(function() {
+        modal = $('.js_trigger-modal').iptoolsModal();
+      });
+      
+      it('expected to construct object', function() {
+        return expect(modal).to.exist;
+      });
 
-  describe('init', function() {
-    
-    beforeEach(function() {
-      modal = $('.js_trigger-modal').iptoolsModal();
     });
-    
-    it('expected to construct object', function() {
-      return expect(modal).to.exist;
-    });
-
   });
-});
+})();
