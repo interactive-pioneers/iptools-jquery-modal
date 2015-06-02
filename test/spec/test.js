f1b3cbf590f28edcd1ad2201b0fa13cb6146506a
David Lehnen <dl@interactive-pioneers.de>
2015-06-02 12:50:48 +0200

    Apply naming convention on test files and bower.json

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,20 +1,17 @@
 'use strict';
 /* jshint undef: false */
 (function() {
-  describe('iptoolsModal', function() {
+  describe('iptModal', function() {
 
     var modal = null;
 
     describe('init', function() {
-      
       beforeEach(function() {
-        modal = $('.js_trigger-modal').iptoolsModal();
+        modal = $('.js_trigger-modal').iptModal();
       });
-      
       it('expected to construct object', function() {
         return expect(modal).to.exist;
       });
-
     });
   });
 })();
