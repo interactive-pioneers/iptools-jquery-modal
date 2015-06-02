8b171e153c5613fcca4c401e07cf6026db6631d2
David Lehnen <dl@interactive-pioneers.de>
2015-06-02 15:47:20 +0200

    Add tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,15 +3,43 @@
 (function() {
   describe('iptModal', function() {
 
-    var modal = null;
+    var config = {
+      height: 500,
+      width: 500,
+      modalClass: 'test'
+    };
+
+    var pluginName = 'plugin_iptModal';
+
+    var object = null;
 
     describe('init', function() {
+
       beforeEach(function() {
-        modal = $('.js_trigger-modal').iptModal();
+        object = $('.js_trigger-modal').iptModal(config);
       });
+
       it('expected to construct object', function() {
-        return expect(modal).to.exist;
+        return expect(object).to.be.an.object;
       });
+
+      it('expected to set height to ' + config.height, function() {
+        return expect(object.data(pluginName).settings.height).to.equal(config.height);
+      });
+
+      it('expected to set width to ' + config.width, function() {
+        return expect(object.data(pluginName).settings.width).to.equal(config.width);
+      });
+
+      it('expected to set modalClass to ' + config.modalClass, function() {
+        return expect(object.data(pluginName).settings.modalClass).to.equal(config.modalClass);
+      });
+
+      it('expected to have class tooltip--active', function() {
+        object.trigger('click');
+        return expect(object.data(pluginName).$modal.hasClass('modal--active')).to.be.ok;
+      });
+
     });
   });
 })();
