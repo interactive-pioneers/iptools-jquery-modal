134a35922173b75032ba442615f3b553c33f2c54
Ain Tohvri <at@interactive-pioneers.de>
2015-07-22 14:50:07 +0200

    Add support for multiple instances on same page

--- src/iptools-jquery-modal.js
+++ src/iptools-jquery-modal.js
@@ -4,336 +4,327 @@
 
   var pluginName = 'iptModal';
 
-  /*
-   * Component handles modals of 2 types:
-   *   - Static. Detect container by ID of href hash.
-   *   - Dynamic. Create container on the fly for URI href.
-   *   - Unobtrusive. Create container on the fly for jquery-ujs.
-   */
-  var TYPES = {
-    STATIC: 'static',
-    DYNAMIC: 'dynamic',
-    UNOBTRUSIVE: 'unobtrusive'
-  };
-
-  var classes = {
-    activeModifier: '--active',
-    effectModifierPrefix: '--effect-',
-    modal: 'modal',
-    spinnerModifier: '--default',
-    elements: {
-      closeButton: '__button-close',
-      spinner: '__spinner'
-    }
-  };
-
-  var dataAttributes = {
-    effect: 'modal-effect'
-  };
-
-  var defaults = {
-    width: 'auto',
-    height: 'auto',
-    zIndex: 102,
-    closeOnESC: true,
-    closeOnClickOutside: true,
-    closeButton: true,
-    modalClass: classes.modal,
-    showSpinner: true,
-    spinnerClass: classes.elements.spinner + classes.spinnerModifier,
-    spinnerHTML: '',
-    modalId: classes.modal
-  };
-
-  var $modal = null;
-  var $spinner = null;
-  var $closeButton = null;
-
-  var loaded = false;
-  var settings = null;
-  var contentLink = null;
-  var effect = null;
-  var type = TYPES.STATIC;
-
-  // FIXME: bind() instead
-  var self = null;
-
-  /**
-   * IPTModal.
-   * @constructor
-   * @param {object} element - jQuery element
-   * @param {object} options - plugin options
-   */
   function IPTModal(element, options) {
+
+    /*
+     * Component handles modals of 2 types:
+     *   - Static. Detect container by ID of href hash.
+     *   - Dynamic. Create container on the fly for URI href.
+     *   - Unobtrusive. Create container on the fly for jquery-ujs.
+     */
+    var TYPES = {
+      STATIC: 'static',
+      DYNAMIC: 'dynamic',
+      UNOBTRUSIVE: 'unobtrusive'
+    };
+
+    var classes = {
+      activeModifier: '--active',
+      effectModifierPrefix: '--effect-',
+      modal: 'modal',
+      spinnerModifier: '--default',
+      elements: {
+        closeButton: '__button-close',
+        spinner: '__spinner'
+      }
+    };
+
+    var dataAttributes = {
+      effect: 'modal-effect'
+    };
+
+    var defaults = {
+      width: 'auto',
+      height: 'auto',
+      zIndex: 102,
+      closeOnESC: true,
+      closeOnClickOutside: true,
+      closeButton: true,
+      modalClass: classes.modal,
+      showSpinner: true,
+      spinnerClass: classes.elements.spinner + classes.spinnerModifier,
+      spinnerHTML: '',
+      modalId: classes.modal
+    };
 
     this.element = $(element);
 
-    // FIXME: bind() instead
-    self = this;
+    var settings = $.extend({}, defaults, options);
+    var contentLink = null;
+    var $spinner = null;
+    var $closeButton = null;
+    var $modal = null;
+    var effect = null;
+    var loaded = false;
+    var type = TYPES.STATIC;
+    var self = this;
 
-    settings = $.extend({}, defaults, options);
+    this.getSettings = function() {
+      return settings;
+    };
 
-    contentLink = this.element.attr('href');
-    $modal = buildModal({link: contentLink});
+    this.getModal = function() {
+      return $modal;
+    };
 
-    // FIXME: effects flattened on multiple instances
-    effect = this.element.data(dataAttributes.effect);
-    if (effect) {
-      $modal.addClass(settings.modalClass + classes.effectModifierPrefix + effect);
-    }
+    this.getEffect = function() {
+      return effect;
+    };
 
-    addEventListeners();
-  }
+    this.active = function() {
+      return $modal.hasClass(classes.modal + classes.activeModifier);
+    };
 
-  IPTModal.prototype.getSettings = function() {
-    return settings;
-  };
+    this.destroy = function() {
+      unbindTemporaryEvents();
+      unbindUnobtrusiveEvents();
+      this.element.off(getNamespacedEvent('click')).removeData('plugin_' + pluginName);
+      removeAllModals();
+    };
 
-  IPTModal.prototype.getModal = function() {
-    return $modal;
-  };
-
-  IPTModal.prototype.active = function() {
-    return $modal.hasClass(classes.modal + classes.activeModifier);
-  };
-
-  IPTModal.prototype.destroy = function() {
-    unbindTemporaryEvents();
-    unbindUnobtrusiveEvents();
-    this.element.off(getNamespacedEvent('click')).removeData('plugin_' + pluginName);
-    removeAllModals();
-  };
-
-  IPTModal.prototype.open = function(data) {
-    if (!data) {
-      throw new Error('Data for modal launch missing!');
-    } else if (!data.link) {
-      throw new Error('Link for modal content missing!');
-    }
-    $modal = buildModal(data);
-    triggerReady();
-    switch (type) {
-      case TYPES.STATIC:
-        triggerSuccess();
-        break;
-      case TYPES.DYNAMIC:
-        $.get(contentLink).done(function(html) {
-          $modal.html(html);
-          triggerSuccess();
-        }).fail(function() {
-          triggerError();
-        });
-        break;
-      case TYPES.UNOBTRUSIVE:
-        triggerReady();
-        bindUnobtrusiveEvents();
-        break;
-      default:
-        throw new Error('Erroneous modal type!');
-    }
-  };
-
-  IPTModal.prototype.close = function() {
-    hide();
-    unbindTemporaryEvents();
-  };
-
-  function triggerReady() {
-    loaded = false;
-    showSpinner();
-    self.element.trigger(getNamespacedEvent('ready'));
-  }
-
-  function triggerComplete() {
-    hideSpinner();
-    self.element.trigger(getNamespacedEvent('complete'));
-  }
-
-  function triggerSuccess() {
-    triggerComplete();
-    loaded = true;
-    addCloseButton();
-    bindTemporaryEvents();
-    show();
-    self.element.trigger(getNamespacedEvent('success'));
-  }
-
-  function triggerError() {
-    triggerComplete();
-    self.element.trigger(getNamespacedEvent('error'));
-  }
-
-  function getNamespacedEvent(name) {
-    return name + '.' + pluginName;
-  }
-
-  function show() {
-    center();
-    $modal.addClass(settings.modalClass + classes.activeModifier);
-  }
-
-  function hide() {
-    $modal.removeClass(settings.modalClass + classes.activeModifier);
-  }
-
-  function addEventListeners() {
-    self.element.on(getNamespacedEvent('click'), handleModalLinkClicked);
-  }
-
-  function buildModal(data) {
-    removeAllModals();
-    if (isStaticModalRequest(data)) {
-      if ($(data.link).length === 0) {
-        throw new Error('Modal content not found!');
+    this.open = function(data) {
+      if (!data) {
+        throw new Error('Data for modal launch missing!');
+      } else if (!data.link) {
+        throw new Error('Link for modal content missing!');
       }
-      type = TYPES.STATIC;
-      return $(data.link)
-        .addClass(settings.modalClass)
-        .data('type', type);
-    }
-    type = isUnobtrusiveModalRequest(data) ? TYPES.UNOBTRUSIVE : TYPES.DYNAMIC;
-    return $('<div/>', {
-        id: settings.modalId,
-        width: settings.width,
-        height: settings.height,
-        class: settings.modalClass
-      })
-      .data('type', type)
-      .appendTo('body');
-  }
-
-  function removeAllModals() {
-    $('.' + settings.modalClass).each(function(index, element) {
-      switch ($(element).data('type')) {
+      $modal = buildModal(data);
+      triggerReady();
+      switch (type) {
         case TYPES.STATIC:
-          $(element).removeClass(settings.modalClass);
+          triggerSuccess();
+          break;
+        case TYPES.DYNAMIC:
+          $.get(contentLink).done(function(html) {
+            $modal.html(html);
+            triggerSuccess();
+          }).fail(function() {
+            triggerError();
+          });
+          break;
+        case TYPES.UNOBTRUSIVE:
+          triggerReady();
+          bindUnobtrusiveEvents();
           break;
         default:
-          $(element).remove();
+          throw new Error('Erroneous modal type!');
       }
-    });
-  }
+    };
 
-  function isStaticModalRequest(data) {
-    return data.link.charAt(0) === '#' && !data.unobtrusive;
-  }
+    this.close = function() {
+      hide();
+      unbindTemporaryEvents();
+    };
 
-  function isUnobtrusiveModalRequest(data) {
-    return data.unobtrusive ? true : false;
-  }
-
-  function addCloseButton() {
-    if (settings.closeButton) {
-      $closeButton = $('<div/>')
-        .addClass(settings.modalClass + classes.elements.closeButton)
-        .appendTo($modal);
+    function unbindUnobtrusiveEvents() {
+      self.element.off('ajax:complete ajax:success ajax:error');
     }
-  }
 
-  function showSpinner() {
-    if (settings.showSpinner) {
-      if (!$spinner) {
-        $spinner = $('<div/>')
-          .addClass(settings.modalClass + classes.elements.spinner)
-          .addClass(settings.spinnerClass)
-          .append(settings.spinnerHTML);
+    function unbindTemporaryEvents() {
+      $(document).off(getNamespacedEvent('keydown') + ' ' + getNamespacedEvent('mouseup'));
+      $(window).off(getNamespacedEvent('resize'));
+    }
+
+    function addEventListeners() {
+      self.element.on(getNamespacedEvent('click'), handleModalLinkClicked);
+    }
+
+    function handleModalLinkClicked(event) {
+      event.preventDefault();
+      var $trigger = $(event.currentTarget);
+      self.open({
+        link: $trigger.attr('href'),
+        unobtrusive: $trigger.data('remote')
+      });
+    }
+
+    function removeAllModals() {
+      $('.' + settings.modalClass).each(function(index, element) {
+        switch ($(element).data('type')) {
+          case TYPES.STATIC:
+            $(element).removeClass(settings.modalClass);
+            break;
+          default:
+            $(element).remove();
+        }
+      });
+    }
+
+    function buildModal(data) {
+      removeAllModals();
+      if (isStaticModalRequest(data)) {
+        if ($(data.link).length === 0) {
+          throw new Error('Modal content not found!');
+        }
+        type = TYPES.STATIC;
+        return $(data.link)
+          .addClass(settings.modalClass)
+          .data('type', type);
       }
-      $('body').append($spinner);
-      $spinner.show();
-    }
-  }
-
-  function hideSpinner() {
-    if ($spinner) {
-      $spinner.hide();
-      $spinner.remove();
-    }
-  }
-
-  function center() {
-    $modal.css({
-      display: 'block',
-      position: 'fixed',
-      top: '50%',
-      left: '50%',
-      marginTop: -$modal.outerHeight() * 0.5,
-      marginLeft: -$modal.outerWidth() * 0.5,
-      zIndex: settings.zIndex
-    });
-  }
-
-  function bindTemporaryEvents() {
-    if ($closeButton) {
-      $closeButton.on(getNamespacedEvent('click'), self.close);
+      type = isUnobtrusiveModalRequest(data) ? TYPES.UNOBTRUSIVE : TYPES.DYNAMIC;
+      return $('<div/>', {
+          id: settings.modalId,
+          width: settings.width,
+          height: settings.height,
+          class: settings.modalClass
+        })
+        .data('type', type)
+        .appendTo('body');
     }
 
-    if (settings.closeOnESC) {
-      $(document).on(getNamespacedEvent('keydown'), handleKeyDown);
+    function triggerReady() {
+      loaded = false;
+      showSpinner();
+      self.element.trigger(getNamespacedEvent('ready'));
     }
 
-    if (settings.closeOnClickOutside) {
-      $(document).on(getNamespacedEvent('mouseup'), handleBodyClick);
+    function triggerComplete() {
+      hideSpinner();
+      self.element.trigger(getNamespacedEvent('complete'));
     }
 
-    $(window).on(getNamespacedEvent('resize'), handleResize);
-  }
-
-  function unbindTemporaryEvents() {
-    $(document).off(getNamespacedEvent('keydown') + ' ' + getNamespacedEvent('mouseup'));
-    $(window).off(getNamespacedEvent('resize'));
-  }
-
-  function bindUnobtrusiveEvents() {
-    self.element.on('ajax:complete', handleUnobtrusiveAjaxComplete)
-      .on('ajax:success', handleUnobtrusiveAjaxSuccess)
-      .on('ajax:error', handleUnobtrusiveAjaxError);
-  }
-
-  function unbindUnobtrusiveEvents() {
-    self.element.off('ajax:complete ajax:success ajax:error');
-  }
-
-  function handleUnobtrusiveAjaxComplete() {
-    triggerComplete();
-  }
-
-  function handleUnobtrusiveAjaxSuccess() {
-    triggerSuccess();
-  }
-
-  function handleUnobtrusiveAjaxError() {
-    triggerError();
-  }
-
-  function handleKeyDown(event) {
-    if (event.which === 27) {
-      self.close();
+    function triggerSuccess() {
+      triggerComplete();
+      loaded = true;
+      addCloseButton();
+      bindTemporaryEvents();
+      show();
+      self.element.trigger(getNamespacedEvent('success'));
     }
-  }
 
-  function handleBodyClick(event) {
-    if (!$modal.is(event.target) && $modal.has(event.target).length === 0) {
-      self.close();
+    function triggerError() {
+      triggerComplete();
+      self.element.trigger(getNamespacedEvent('error'));
     }
-  }
 
-  function handleResize(event) {
-    var target = event.data;
-    if (typeof(target.resizeTimeout) !== 'undefined') {
-      clearTimeout(target.resizeTimeout);
+    function getNamespacedEvent(name) {
+      return name + '.' + pluginName;
     }
-    target.resizeTimeout = setTimeout(function() {
+
+    function show() {
       center();
-    }, 250);
-  }
+      $modal.addClass(settings.modalClass + classes.activeModifier);
+    }
 
-  function handleModalLinkClicked(event) {
-    event.preventDefault();
-    var $trigger = $(event.currentTarget);
-    self.open({
-      link: $trigger.attr('href'),
-      unobtrusive: $trigger.data('remote')
-    });
+    function hide() {
+      $modal.removeClass(settings.modalClass + classes.activeModifier);
+    }
+
+    function isStaticModalRequest(data) {
+      return data.link && data.link.charAt(0) === '#' && !data.unobtrusive;
+    }
+
+    function isUnobtrusiveModalRequest(data) {
+      return data.unobtrusive ? true : false;
+    }
+
+    function addCloseButton() {
+      if (settings.closeButton) {
+        $closeButton = $('<div/>')
+          .addClass(settings.modalClass + classes.elements.closeButton)
+          .appendTo($modal);
+      }
+    }
+
+    function showSpinner() {
+      if (settings.showSpinner) {
+        if (!$spinner) {
+          $spinner = $('<div/>')
+            .addClass(settings.modalClass + classes.elements.spinner)
+            .addClass(settings.spinnerClass)
+            .append(settings.spinnerHTML);
+        }
+        $('body').append($spinner);
+        $spinner.show();
+      }
+    }
+
+    function hideSpinner() {
+      if ($spinner) {
+        $spinner.hide();
+        $spinner.remove();
+      }
+    }
+
+    function center() {
+      $modal.css({
+        display: 'block',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        marginTop: -$modal.outerHeight() * 0.5,
+        marginLeft: -$modal.outerWidth() * 0.5,
+        zIndex: settings.zIndex
+      });
+    }
+
+    function bindTemporaryEvents() {
+      if ($closeButton) {
+        $closeButton.on(getNamespacedEvent('click'), self.close);
+      }
+
+      if (settings.closeOnESC) {
+        $(document).on(getNamespacedEvent('keydown'), handleKeyDown);
+      }
+
+      if (settings.closeOnClickOutside) {
+        $(document).on(getNamespacedEvent('mouseup'), handleBodyClick);
+      }
+
+      $(window).on(getNamespacedEvent('resize'), handleResize);
+    }
+
+    function bindUnobtrusiveEvents() {
+      self.element.on('ajax:complete', handleUnobtrusiveAjaxComplete)
+        .on('ajax:success', handleUnobtrusiveAjaxSuccess)
+        .on('ajax:error', handleUnobtrusiveAjaxError);
+    }
+
+    function handleUnobtrusiveAjaxComplete() {
+      triggerComplete();
+    }
+
+    function handleUnobtrusiveAjaxSuccess() {
+      triggerSuccess();
+    }
+
+    function handleUnobtrusiveAjaxError() {
+      triggerError();
+    }
+
+    function handleKeyDown(event) {
+      if (event.which === 27) {
+        self.close();
+      }
+    }
+
+    function handleBodyClick(event) {
+      if (!$modal.is(event.target) && $modal.has(event.target).length === 0) {
+        self.close();
+      }
+    }
+
+    function handleResize(event) {
+      var target = event.data;
+      if (typeof(target.resizeTimeout) !== 'undefined') {
+        clearTimeout(target.resizeTimeout);
+      }
+      target.resizeTimeout = setTimeout(function() {
+        center();
+      }, 250);
+    }
+
+    function init() {
+      effect = self.element.data(dataAttributes.effect);
+      contentLink = self.element.attr('href');
+      $modal = buildModal({link: contentLink});
+      if (effect) {
+        $modal.addClass(settings.modalClass + classes.effectModifierPrefix + effect);
+      }
+      addEventListeners();
+    }
+
+    init();
   }
 
   $.fn[pluginName] = function(options) {
--- test/spec/test.js
+++ test/spec/test.js
@@ -36,6 +36,47 @@
         return expect(object.data(pluginName).getSettings().width).to.equal(config.width);
       });
 
+      context('when multiple instances', function() {
+
+        var config2 = {
+          height: 600,
+          width: 600,
+          modalClass: 'test2',
+          modalId: 'test2'
+        };
+        var object2 = null;
+        var selector2 = '.js_trigger-modal2';
+
+        before(function() {
+          object2 = $(selector2).iptModal(config2);
+        });
+
+        after(function() {
+          object2.data(pluginName).destroy();
+        });
+
+        it('expected to construct 2 objects', function() {
+          return expect(object.data(pluginName)).to.be.an.object && expect(object2.data(pluginName)).to.be.an.object;
+        });
+
+        it('expected to set 1st instance width to ' + config.width, function() {
+          return expect(object.data(pluginName).getSettings().width).to.equal(config.width);
+        });
+
+        it('expected to set 2nd instance width to ' + config2.width, function() {
+          return expect(object2.data(pluginName).getSettings().width).to.equal(config2.width);
+        });
+
+        it('expected to have correct 1st instance effect', function() {
+          return expect(object.data(pluginName).getEffect()).to.equal($(selector).data('modal-effect'));
+        });
+
+        it('expected to have correct 2nd instance effect', function() {
+          return expect(object2.data(pluginName).getEffect()).to.equal($(selector2).data('modal-effect'));
+        });
+
+      });
+
     });
 
     describe('open', function() {
@@ -210,8 +251,14 @@
         });
 
         it('expected to have content post success event', function(done) {
+          var mockConfig = {
+            height: 500,
+            width: 500,
+            modalClass: 'test'
+          };
+          object = $(selector).attr('href', 'dummy.html').iptModal(mockConfig);
           object.on('success.iptModal', function() {
-            expect($('.' + config.modalClass).html()).to.match(/<h1>Dummy content<\/h1>/);
+            expect($('.' + mockConfig.modalClass).html()).to.match(/<h1>Dummy content<\/h1>/);
             done();
           }).trigger('click');
         });
